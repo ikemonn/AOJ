fa38015a24244df55ac1bbc576d675debed06407
ikemoto <ikemoto.daiki@gmail.com>
2014-07-04 00:03:05 +0900

    :golf

--- structured_program2/spreadsheet.rb
+++ structured_program2/spreadsheet.rb
@@ -1,25 +1,5 @@
-r, c = gets.split.map(&:to_i)
-cell = []
-r.times do |num|
-  cell[num] = gets.chomp.split.map(&:to_i)
-end
-
-cell[r] = []
-# 列の合計
-c.times do |col|
-  col_sum = 0
-  r.times do |row|
-    col_sum += cell[row][col]
-  end
-  cell[r][col] = col_sum
-end
-
-# 行の合計
-(r + 1).times do |row|
-  row_sum = 0
-  c.times do |col|
-    row_sum += cell[row][col]
-  end
-  cell[row] << row_sum
-  puts cell[row].join(" ")
-end
+a = $<.drop(1).map{|e| e.split.map(&:to_i)}
+a.each{|e| e << e.reduce(:+)}
+a = a.transpose
+a.each{|e| e << e.reduce(:+)}
+a.transpose.map{|e| puts e * " "}
