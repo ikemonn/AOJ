2b6831d6dd030120ab57a94444c689308db40a34
ikemoto <ikemoto.daiki@gmail.com>
2014-07-05 16:48:09 +0900

    ITP1_8_C解いた

--- character/counting_characters.rb
+++ character/counting_characters.rb
@@ -1,9 +1,14 @@
-loop do
-  char = gets
-  p char
-  break if char.nil?
+ans = {}
+("a".."z").each do |s|
+  ans[s] = 0
 end
-  "a".upto("z") do |alpha|
-    num = char.chomp.scan(/[#{alpha}]/i).size
-    puts "#{alpha} : #{num}"
+
+while (ln = gets) do
+  ln.downcase.gsub(/[^a-z]/, '').chars do |key|
+    ans[key] += 1
   end
+end
+
+ans.each do |key, val|
+  puts "#{key} : #{val}"
+end
