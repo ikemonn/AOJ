dabab41334553953a13984a728b6f3a3116a751b
ikemoto <ikemoto.daiki@gmail.com>
2014-07-06 21:11:01 +0900

    :golf:

--- math_functions/standard_deviation.rb
+++ math_functions/standard_deviation.rb
@@ -4,11 +4,5 @@ while (n = gets.chomp.to_f) != 0 do
   s = gets.chomp.split.map(&:to_f)
   avg = s.reduce(:+) / n
   sum = 0
-
-  s.each do |num|
-    sum += (num - avg) ** 2
-  end
-
-  puts sqrt(sum / n)
-
+  puts sqrt(s.map{ |num| (num - avg)**2 }.reduce(:+) / n)
 end
