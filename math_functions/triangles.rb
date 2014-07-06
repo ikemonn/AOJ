ebd8a60f768abd180f2d30e14f73943fc93c6840
ikemoto <ikemoto.daiki@gmail.com>
2014-07-06 11:24:18 +0900

    :golf:

--- math_functions/triangles.rb
+++ math_functions/triangles.rb
@@ -1,8 +1,4 @@
+include Math
 a, b, c = gets.chomp.split.map(&:to_f)
-
-s = (a * b * Math.sin(c / 180 * Math::PI)) / 2
-l = a + b + (Math.sqrt(a**2 + b**2 - 2*a*b*Math.cos(c / 180 * Math::PI)))
-h = 2 * s / a
-puts sprintf("%.12f", s)
-puts sprintf("%.12f", l)
-puts sprintf("%.8f", h)
+c = c * PI / 180
+p a * b * sin(c) / 2, a + b + sqrt(a * a + b * b - 2 * a * b * cos(c)), b * sin(c)
