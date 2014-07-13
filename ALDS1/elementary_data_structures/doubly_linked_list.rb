e54b52883f0c023a7376c2225dc6fe90c105c249
ikemoto <ikemoto.daiki@gmail.com>
2014-07-13 22:16:14 +0900

    ALDS1_3解いた

--- ALDS1/elementary_data_structures/doubly_linked_list.rb
+++ ALDS1/elementary_data_structures/doubly_linked_list.rb
@@ -1,7 +1,20 @@
 array = []
-
 (gets.to_i).times do
-
-
-
+  command, num = gets.split
+  num = num.to_i
+  begin
+    case command
+    when 'insert'
+      array.unshift(num)
+    when 'delete'
+      array.delete_at(array.index(num))
+    when 'deleteFirst'
+      array.shift
+    when 'deleteLast'
+      array.pop
+    end
+  rescue
+    next
+  end
 end
+puts array * " "
